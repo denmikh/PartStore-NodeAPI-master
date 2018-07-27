128eb1231d985b8669695f1a0565b04f264bdb4e
denmikh <denmih61@gmail.com>
2018-07-27 12:09:50 +0300

    Adding in routes, "Create part" and "Update part"

--- libs/routes/parts.js
+++ libs/routes/parts.js
@@ -26,6 +26,45 @@ router.get('/', function (req, res) {
     });
 });
 
+// Create parts
+router.post('/', function (req, res) {
+    console.log('req.body: ', req.body)
+    var part = new Part({
+        name: req.body.name,
+        origArticle: req.body.origArticle,
+        description: req.body.description,
+        fullDescription: req.body.fullDescription,
+        applicability: req.body.applicability,
+        price: req.body.price,
+        image: req.body.image
+    });
+    console.log('part: ', part)
+    part.save(function (err) {
+        if (!err) {
+            log.info('New part created with id: %s', part.id);
+            return res.json({
+                status: 'OK',
+                part: part
+            });
+        } else {
+            if (err.name === 'ValidationError') {
+                res.statusCode = 400;
+                res.json({
+                    error: 'Validation error'
+                });
+            } else {
+                res.statusCode = 500;
+
+                log.error('Internal error(%d): %s', res.statusCode, err.message);
+
+                res.json({
+                    error: 'Server error'
+                });
+            }
+        }
+    });
+});
+
 // Get parts
 router.get('/:id', function (req, res) {
 
@@ -55,5 +94,48 @@ router.get('/:id', function (req, res) {
     });
 });
 
+// Update parts
+router.put('/:id', function (req, res) {
+    var partId = req.params.id;
+
+    Part.findById(artipartIdcleId, function (err, part) {
+        if (!part) {
+            res.statusCode = 404;
+            log.error('part with id: %s Not Found', part);
+            return res.json({
+                error: 'Not found'
+            });
+        }
+
+        part.title = req.body.title;
+        part.description = req.body.description;
+        part.author = req.body.author;
+        part.images = req.body.images;
+
+        part.save(function (err) {
+            if (!err) {
+                log.info('part with id: %s updated', part.id);
+                return res.json({
+                    status: 'OK',
+                    part: part
+                });
+            } else {
+                if (err.name === 'ValidationError') {
+                    res.statusCode = 400;
+                    return res.json({
+                        error: 'Validation error'
+                    });
+                } else {
+                    res.statusCode = 500;
+
+                    return res.json({
+                        error: 'Server error'
+                    });
+                }
+                log.error('Internal error (%d): %s', res.statusCode, err.message);
+            }
+        });
+    });
+});
 
 module.exports = router;
